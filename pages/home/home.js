a9fbf17abc960ba4575666b565cd11e0882a89a9
Paulo José <114526829+PauloJr14@users.noreply.github.com>
2024-07-14 19:56:43 -0300

    Add files via upload

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,22 +1,83 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
-            // Obtém a referência à coleção "inscrito-olimpiadahistoria"
             var collection = firebase.firestore().collection("dados");
 
-            // Obtém o documento do usuário atual
+            // Consulta para contar os documentos onde "inscrito-historia2024" é igual a "Sim"
+            collection.where("inscrito-historia2024", "==", "Sim").get().then((querySnapshot) => {
+                var local = document.getElementById("total");
+                local.textContent = querySnapshot.size
+                var local2 = document.getElementById("ttotal");
+                local2.textContent = querySnapshot.size + 442
+
+            }).catch((error) => {
+                console.error("Erro ao consultar documentos:", error);
+            });
+
+            collection.where("inscrito-historia2024", "==", "Sim").where("level", "==", "nível 1").get().then((querySnapshot) => {
+                var local = document.getElementById("n1");
+                local.textContent = querySnapshot.size; // Número de documentos na consulta
+                var local2 = document.getElementById("tn1");
+                local2.textContent = querySnapshot.size + 80
+
+            }).catch((error) => {
+                console.error("Erro ao consultar documentos:", error);
+            });
+
+            collection.where("inscrito-historia2024", "==", "Sim").where("level", "==", "nível 2").get().then((querySnapshot) => {
+                var local = document.getElementById("n2");
+                local.textContent = querySnapshot.size
+                var local2 = document.getElementById("tn2");
+                local2.textContent = querySnapshot.size + 134
+
+                // Pode adicionar código adicional aqui para usar a contagem como necessário
+            }).catch((error) => {
+                console.error("Erro ao consultar documentos:", error);
+            });
+
+            collection.where("inscrito-historia2024", "==", "Sim").where("level", "==", "nível 3").get().then((querySnapshot) => {
+                var local = document.getElementById("n3");
+                local.textContent = querySnapshot.size
+                var local2 = document.getElementById("tn3");
+                local2.textContent = querySnapshot.size + 228
+
+                // Pode adicionar código adicional aqui para usar a contagem como necessário
+            }).catch((error) => {
+                console.error("Erro ao consultar documentos:", error);
+            });
+
+            collection.where("inscrito-historia2024", "==", "Sim").where("level", "==", "nível 4").get().then((querySnapshot) => {
+                var local = document.getElementById("n4");
+                local.textContent = querySnapshot.size
+                var local2 = document.getElementById("tn4");
+                local2.textContent = querySnapshot.size
+
+                // Pode adicionar código adicional aqui para usar a contagem como necessário
+            }).catch((error) => {
+                console.error("Erro ao consultar documentos:", error);
+            });
+
             collection.doc(user.uid).get().then((doc) => {
                 if (doc.exists) {
-                    // Verifica se o campo "olimpiada-historia" existe no documento
                     if (doc.data().hasOwnProperty("inscrito-historia2024")) {
-                        // Se o campo existir, atualiza a interface do usuário
-                        var h2 = document.getElementById("inscrito");
-                        var button = document.getElementById("inscrever");
+                        var inscritoValor = doc.data()["inscrito-historia2024"];
+                        if (inscritoValor == "Sim") {
+                            var h2 = document.getElementById("inscrito");
+                            var button = document.getElementById("inscrever");
 
-                        h2.style.display = "block";  // Corrigido de "box" para "block"
-                        button.style.display = "none";
+                            h2.style.display = "block";
+                            button.style.display = "none";
+                        } else if (inscritoValor == "admin") {
+                            var button1 = document.getElementById("admin");
+                            var button2 = document.getElementById("inscrever");
+
+                            button1.style.display = "block";
+                            button2.style.display = "none";
+                        } else {
+                            console.log('O campo "inscrito-historia2024" não é igual a "Sim" ou "admin"');
+                        }
                     } else {
-                        console.log('Campo "olimpiada-historia" não encontrado no documento');
+                        console.log('Campo "inscrito-historia2024" não encontrado no documento');
                     }
                 } else {
                     console.log("Documento não encontrado");
@@ -46,11 +107,21 @@ function subscribe() {
     form.style.display = "block";
 }
 
+function admin() {
+    var div1 = document.getElementById("olimpiadas");
+    var div2 = document.getElementById("nav-admin");
+
+    div1.style.display = "none";
+    div2.style.display = "block";
+}
+
 function comeback() {
-    var div = document.getElementById("olimpiadas");
+    var div1 = document.getElementById("olimpiadas");
+    var div2 = document.getElementById("nav-admin");
     var form = document.getElementById("olympiadForm");
 
-    div.style.display = "flex";
+    div1.style.display = "flex";
+    div2.style.display = "none";
     form.style.display = "none";
 }
 
