d21e1f15b1d7db2aa8bb70d055dba15567129491
Paulo José <114526829+PauloJr14@users.noreply.github.com>
2024-07-21 15:21:56 -0300

    Add files via upload

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('textarea').forEach(textarea => {
+        textarea.addEventListener('input', () => ajustarAlturaTextarea(textarea));
+        ajustarAlturaTextarea(textarea); // Ajusta a altura inicial para o conteúdo pré-preenchido
+    });
+
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             var collection = firebase.firestore().collection("dados");
@@ -8,6 +13,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     if (doc.data().hasOwnProperty("inscrito-historia2024")) {
                         var inscritoValor = doc.data()["inscrito-historia2024"];
                         if (inscritoValor == "Sim") {
+                            aluno(user);
                             var button1 = document.getElementById("aluno");
                             var button2 = document.getElementById("inscrever");
 
@@ -67,12 +73,85 @@ function prof() {
     div2.style.display = "block";
 }
 
-function aluno() {
+function navaluno() {
     var div1 = document.getElementById("olimpiadas");
     var div2 = document.getElementById("nav-aluno");
 
     div1.style.display = "none";
     div2.style.display = "block";
+}
+
+function gabarito() {
+    var div1 = document.getElementById("nav-aluno");
+    var div2 = document.getElementById("gabarito");
+
+    div1.style.display = "none";
+    div2.style.display = "block";
+    
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            var collection = firebase.firestore().collection("respostas-historia2024");
+            var userDoc = collection.doc(user.uid);
+
+            userDoc.get().then((doc) => {
+                if (doc.exists) {
+                    var data = doc.data();
+                    
+                    // Preencher respostas objetivas
+                    if (data.respostas && data.respostas.objetivas) {
+                        var respostasObjetivas = data.respostas.objetivas;
+                        for (let key in respostasObjetivas) {
+                            if (respostasObjetivas.hasOwnProperty(key)) {
+                                let radio = document.querySelector(`input[name="${key}"][value="${respostasObjetivas[key]}"]`);
+                                if (radio) {
+                                    radio.checked = true;
+                                }
+                            }
+                        }
+                    }
+
+                    // Preencher respostas abertas
+                    if (data.respostas && data.respostas.dissertativas) {
+                        var respostasDissertativas = data.respostas.dissertativas;
+                        for (let key in respostasDissertativas) {
+                            if (respostasDissertativas.hasOwnProperty(key)) {
+                                let textarea = document.getElementById(key);
+                                if (textarea) {
+                                    textarea.value = respostasDissertativas[key];
+                                    ajustarAlturaTextarea(textarea); // Ajusta a altura inicial para o conteúdo pré-preenchido
+                                }
+                            }
+                        }
+                    }
+                }
+            }).catch((error) => {
+                console.error("Erro ao carregar respostas:", error);
+            });
+        } else {
+            console.log("Nenhum usuário conectado");
+        }
+    });
+}
+
+function aluno(user) {
+    var collection = firebase.firestore().collection("dados");
+
+    collection.doc(user.uid).get().then((doc) => {
+        var inscritoValor = doc.data() ? doc.data()["level"] : null; // Verifica se data existe
+        if (inscritoValor) {
+            var button = document.getElementById(`prova${inscritoValor.charAt(inscritoValor.length - 1)}`);
+
+            if (button) {
+                button.style.display = "block";
+            } else {
+                console.error(`Botão para ${inscritoValor} não encontrado.`);
+            }
+        } else {
+            console.error("Valor de 'level' não encontrado no documento.");
+        }
+    }).catch((error) => {
+        console.error("Erro ao acessar o Firestore:", error);
+    });
 }
 
 function admin() {
@@ -81,64 +160,30 @@ function admin() {
 
     div1.style.display = "none";
     div2.style.display = "block";
-
-    var collection = firebase.firestore().collection("dados");
-
-    collection.get().then((querySnapshot) => {
-        var totalSim = 0;
-        var nivel1Count = 0;
-        var nivel2Count = 0;
-        var nivel3Count = 0;
-        var nivel4Count = 0;
-
-        querySnapshot.forEach((doc) => {
-            var data = doc.data();
-            if (data["inscrito-historia2024"] === "Sim") {
-                totalSim++;
-                switch (data["level"]) {
-                    case "nível 1":
-                        nivel1Count++;
-                        break;
-                    case "nível 2":
-                        nivel2Count++;
-                        break;
-                    case "nível 3":
-                        nivel3Count++;
-                        break;
-                    case "nível 4":
-                        nivel4Count++;
-                        break;
-                }
-            }
-        });
-
-        document.getElementById("total").textContent = totalSim;
-        document.getElementById("ttotal").textContent = totalSim + 442;
-
-        document.getElementById("n1").textContent = nivel1Count;
-        document.getElementById("tn1").textContent = nivel1Count + 80;
-
-        document.getElementById("n2").textContent = nivel2Count;
-        document.getElementById("tn2").textContent = nivel2Count + 134;
-
-        document.getElementById("n3").textContent = nivel3Count;
-        document.getElementById("tn3").textContent = nivel3Count + 228;
-
-        document.getElementById("n4").textContent = nivel4Count;
-        document.getElementById("tn4").textContent = nivel4Count;
-    }).catch((error) => {
-        console.error("Erro ao consultar documentos:", error);
-    });
 }
 
 function comeback() {
     var div1 = document.getElementById("olimpiadas");
     var div2 = document.getElementById("nav-admin");
+    var div3 = document.getElementById("nav-prof");
+    var div4 = document.getElementById("nav-aluno");
+    var div5 = document.getElementById("gabarito");
     var form = document.getElementById("olympiadForm");
 
     div1.style.display = "flex";
     div2.style.display = "none";
     form.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+}
+
+function comeback2() {
+    var div1 = document.getElementById("nav-aluno");
+    var div2 = document.getElementById("gabarito");
+
+    div1.style.display = "block";
+    div2.style.display = "none";
 }
 
 function subcomplited() {
@@ -174,6 +219,126 @@ function subcomplited() {
     } else {
         alert("Nenhum usuário está conectado");
     }
+}
+
+function rascunho() {
+    var user = firebase.auth().currentUser;
+    if (user) {
+        var collection = firebase.firestore().collection("respostas-historia2024");
+        var userDoc = collection.doc(user.uid);
+
+        userDoc.get().then((doc) => {
+            if (doc.exists) {
+                // Se o documento existe, verifica se o campo "state" está presente
+                if (doc.data().hasOwnProperty("state")) {
+                    alert("Você já entregou as questões objetivas");
+                } else {
+                    // Se o campo "state" não existe, atualiza o rascunho
+                    saveDraft(userDoc);
+                }
+            } else {
+                // Se o documento não existe, cria um novo e salva o rascunho
+                saveDraft(userDoc);
+            }
+        }).catch((error) => {
+            console.error("Erro ao acessar o documento:", error);
+        });
+    } else {
+        alert("Nenhum usuário está conectado");
+    }
+}
+
+function entregar() {
+    var user = firebase.auth().currentUser;
+    if (user) {
+        var collection = firebase.firestore().collection("respostas-historia2024");
+        var userDoc = collection.doc(user.uid);
+
+        // Obtém as respostas das questões objetivas
+        var respostasObjetivas = {};
+        for (var i = 1; i <= 20; i++) { // Supondo que tenha 20 questões objetivas
+            var respostaSelecionada = document.querySelector(`input[name="q${i}"]:checked`);
+            if (respostaSelecionada) {
+                respostasObjetivas[`q${i}`] = respostaSelecionada.value;
+            }
+        }
+
+        // Obtém as respostas das questões dissertativas
+        var respostasDissertativas = {};
+        var numQuestoesDissertativas = 5; // Ajuste conforme o número real de questões dissertativas
+        for (var j = 1; j <= numQuestoesDissertativas; j++) {
+            var respostaTexto = document.getElementById(`essay${j}`).value;
+            respostasDissertativas[`essay${j}`] = respostaTexto;
+        }
+
+        // Combina as respostas objetivas e dissertativas
+        var respostas = {
+            objetivas: respostasObjetivas,
+            dissertativas: respostasDissertativas
+        };
+
+        userDoc.get().then((doc) => {
+            if (doc.exists && doc.data().hasOwnProperty("state")) {
+                alert("Você já entregou as suas respostas");
+            } else {
+                // Atualiza o documento com as respostas e o campo "state"
+                userDoc.set({
+                    respostas: respostas,
+                    state: true
+                }, { merge: true }).then(() => {
+                   alert("Respostas enviadas com sucesso! Obrigado por participar da 1º Olimpíada Arandu de História!");
+                }).catch((error) => {
+                    console.error("Erro ao entregar:", error);
+                });
+            }
+        }).catch((error) => {
+            console.error("Erro ao consultar o documento:", error);
+        });
+    } else {
+        alert("Nenhum usuário está conectado");
+    }
+}
+
+function saveDraft(userDoc) {
+    // Obtém as respostas das questões objetivas
+    var respostasObjetivas = {};
+    for (var i = 1; i <= 20; i++) { // Supondo que tenha 20 questões objetivas
+        var respostaSelecionada = document.querySelector(`input[name="q${i}"]:checked`);
+        if (respostaSelecionada) {
+            respostasObjetivas[`q${i}`] = respostaSelecionada.value;
+        }
+    }
+
+    // Obtém as respostas das questões dissertativas
+    var respostasDissertativas = {};
+    var numQuestoesDissertativas = 5; // Ajuste conforme o número real de questões dissertativas
+    for (var j = 1; j <= numQuestoesDissertativas; j++) {
+        var respostaTexto = document.getElementById(`essay${j}`).value;
+        respostasDissertativas[`essay${j}`] = respostaTexto;
+    }
+
+    // Combina as respostas objetivas e dissertativas
+    var respostas = {
+        objetivas: respostasObjetivas,
+        dissertativas: respostasDissertativas
+    };
+
+    userDoc.get().then((doc) => {
+        if (doc.exists && doc.data().hasOwnProperty("state")) {
+            alert("Você já entregou as suas respostas");
+        } else {
+            // Atualiza o documento com as respostas e o campo "state"
+            userDoc.set({
+                respostas: respostas,
+            }, { merge: true }).then(() => {
+                alert("Rascunho salvo com sucesso!");
+            }).catch((error) => {
+                console.error("Erro ao entregar:", error);
+            });
+        }
+    }).catch((error) => {
+        console.error("Erro ao consultar o documento:", error);
+    });
 }
 
 document.getElementById("copyEmailsButton").addEventListener("click", async () => {
@@ -215,3 +380,75 @@ document.getElementById("copyEmailsButton").addEventListener("click", async () =
         console.error("Erro ao recuperar e copiar os e-mails:", error);
     }
 });
+
+function prova1() {
+    window.location.href = "https://drive.google.com/file/d/1bk75pURSJXuqaZvl-lw80Oe5RGgBq5be/view?usp=sharing";
+}
+
+function prova2() {
+    window.location.href = "https://drive.google.com/file/d/1tQPN_Z9fYldZnwPywUA1yFnWmkO9aNBb/view?usp=sharing";
+}
+
+function prova3() {
+    window.location.href = "https://drive.google.com/file/d/1hvzYPAT6ObqLdwDK0lhWmyIBhcrVnimB/view?usp=sharing";
+}
+
+function prova4() {
+    window.location.href = "https://drive.google.com/file/d/1ayZAtpw9nnkap82BXkGLAZMF_-x9rsRL/view?usp=sharing";
+}
+
+function ajustarAlturaTextarea(textarea) {
+    if (textarea.value == "") {
+        textarea.style.height = "50px"; // Redefine a altura para calcular o novo tamanho
+    } else {
+        textarea.style.height = "auto"; // Redefine a altura para calcular o novo tamanho
+        textarea.style.height = textarea.scrollHeight + "px"; // Ajusta a altura de acordo com o conteúdo
+    }
+}
+
+function uploadFile() {
+    var fileInput = document.getElementById('file-input');
+    var file = fileInput.files[0];
+
+    if (file) {
+        // Referência para o armazenamento do Firebase
+        var storageRef = firebase.storage().ref();
+        var user = firebase.auth().currentUser;
+
+        if (user) {
+            var fileRef = storageRef.child('uploads/' + user.uid + '/' + file.name);
+            var uploadTask = fileRef.put(file);
+
+            // Monitorar o progresso do upload
+            uploadTask.on('state_changed', function(snapshot) {
+                var porcentagem = document.getElementById("porcentagem");
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                porcentagem.innerText = "Progresso do upload: " + progress.toFixed(2) + "%";
+            }, function(error) {
+                // Tratar erros no upload
+                console.error('Erro ao fazer upload:', error);
+                alert('Erro ao fazer upload do arquivo.');
+            }, function() {
+                // Upload concluído com sucesso
+                uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                    console.log('Arquivo disponível em:', downloadURL);
+                    alert('Arquivo enviado com sucesso! URL: ' + downloadURL);
+
+                    // Aqui você pode salvar a URL do download no Firestore se necessário
+                    var userDoc = firebase.firestore().collection('uploads').doc(user.uid);
+                    userDoc.set({
+                        fileURL: downloadURL
+                    }, { merge: true }).then(() => {
+                        console.log('URL do arquivo salvo no Firestore com sucesso.');
+                    }).catch((error) => {
+                        console.error('Erro ao salvar URL do arquivo no Firestore:', error);
+                    });
+                });
+            });
+        } else {
+            console.log('Nenhum usuário conectado.');
+        }
+    } else {
+        alert('Por favor, selecione um arquivo.');
+    }
+}
